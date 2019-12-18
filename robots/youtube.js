6ccb7c8fac416ea3634828bc9af552bb967c3906
Lai Xuancheng <lai32690@gmail.com>
2019-12-17 21:38:32 -0300

    creating OAuth

--- robots/youtube.js
+++ robots/youtube.js
@@ -1,7 +1,93 @@
-const path = require('path');
-const fs = require('fs');
+const express = require('express');
+const google = require('googleapis').google;
+const OAuth2 = google.auth.OAuth2;
 
 async function robot() {
+    await authenticateOAuth();
+    // await updateVideo();
+    // await updateDescription();
+
+    async function authenticateOAuth() {
+        const webServer = await startWebServer();
+        const OAuthClient = await createOAuthClient();
+        requestUserConsent(OAuthClient);
+        const authToken = await waitForGoogleCallback(webServer);
+        await requestAccessTokens(OAuthClient, authToken);
+        setGlobalGoogleAuthentication(OAuthClient);
+        await stopWebServer(webServer);
+
+        async function startWebServer() {
+            const port = 5000;
+            return new Promise((resolve, reject) => {
+                const app = express();
+                const server = app.listen(port, () => {
+                    console.log('Server is on port 5000');
+                    resolve({
+                        app,
+                        server
+                    });
+                });
+            });
+        }
+
+        async function createOAuthClient() {
+            const credentials = require('../credentials/video-generator-oauth.json');
+
+            const OAuthClient = new OAuth2(
+                credentials.web.client_id,
+                credentials.web.client_secret,
+                credentials.web.redirect_uris[0]
+            );
+
+            return OAuthClient;
+        }
+
+        function requestUserConsent(OAuthClient) {
+            const consentUrl = OAuthClient.generateAuthUrl({
+                access_type: 'offline',
+                scope: ['https://www.googleapis.com/auth/youtube']
+            });
+
+            console.log(`Please give your consent: ${consentUrl}`);
+        }
+
+        async function waitForGoogleCallback(webServer) {
+            return new Promise((resolve, reject) => {
+                console.log('Waiting for user consent...');
+                webServer.app.get('/oauth-callback', (req, res) => {
+                    const authToken = req.query.code;
+                    console.log(`Consent given: ${authCode}`);
+                    res.send('<h1>Thank you!</h1>Now you can close this page.');
+                    resolve(authToken);
+                });
+            });
+        }
+
+        async function requestAccessTokens(OAuthClient, authToken) {
+            return new Promise((resolve, reject) => {
+                OAuthClient.getToken(authToken, (error, tokens) => {
+                    if(error) reject(error);
+
+                    console.log('Access token received.');
+
+                    OAuthClient.setCredentials(tokens);
+                    resolve();
+                });
+            });
+        }
+
+        function setGlobalGoogleAuthentication(OAuthClient) {
+            google.options({
+                auth: OAuthClient
+            });
+        }
+
+        async function stopWebServer(webServer) {
+            return new Promise(resolve => {
+                webServer.server.close(resolve);
+            });
+        }
+    }
 }
 
 module.exports = robot;
